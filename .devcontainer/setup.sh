83527ae8544ca42b458617d561608b3789e69cc0
michdr <michael.druk@mostly.ai>
2025-02-05 11:10:32 +0100

    wip

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,9 +6,8 @@ pip install uv
 uv venv
 uv sync --extra dev --frozen
 
-# Activate venv explicitly before installing ipykernel
 source .venv/bin/activate
-pip install --upgrade --force-reinstall ipykernel
+uv pip install --upgrade --force-reinstall pip ipykernel
 
 # Register the kernel explicitly (optional, but ensures VS Code detects it)
 python -m ipykernel install --user
