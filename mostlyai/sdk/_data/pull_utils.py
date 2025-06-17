162c8fc9f1801c72ff91b6a969ed40daef375a6d
Lukasz Kolodziejczyk <lukasz.kolodziejczyk@mostly.ai>
2025-06-17 18:13:51 +0200

    fix: excluded PK on DB table

--- mostlyai/sdk/_data/pull_utils.py
+++ mostlyai/sdk/_data/pull_utils.py
@@ -287,7 +287,7 @@ def export_meta(
     tgt_metadata_path = workspace_dir / "OriginalData" / "tgt-meta"
     tgt_metadata_path.mkdir(parents=True, exist_ok=True)
     # tgt-meta / keys.json
-    tgt_pk = schema.get_primary_key(tgt).column
+    tgt_pk = pk.column if (pk := schema.get_primary_key(tgt)) else None
     for rel in schema.subset(relations_to=tgt).relations:
         if rel.child == DataIdentifier(tgt, tgt_pk):
             # having the same field as PK and FK is not supported
